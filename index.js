e82eadaefbf5d399d687221fb79076c342c73960
hyungji24 <hyungjikim@email.arizona.edu>
2022-12-20 22:11:24 +0900

    Challenge 4 of 7: Create the summary statistics at the top of the page

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function addGamesToPage(games) {
         div.innerHTML = `
             <img class='game-img' src=${game.img} alt=${game.name}/>
             <h3>${game.name}</h3>
-            <p><strong>$${game.pledged}</strong> pledged by <strong>${game.backers}</strong> gamers<p>
+            <p><strong>$${game.pledged.toLocaleString('en-US')}</strong> pledged by <strong>${game.backers}</strong> gamers<p>
             <p>${game.description}</p>
         `
 
@@ -67,20 +67,21 @@ addGamesToPage(GAMES_JSON)
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce((acc, game) => acc + game.backers, 0)
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${totalContributions.toLocaleString('en-US')}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+const totalRaised = GAMES_JSON.reduce((acc, game) => acc + game.pledged, 0)
 
 // set inner HTML using template literal
-
+raisedCard.innerHTML = `$${totalRaised.toLocaleString('en-US')}`
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
-
+gamesCard.innerHTML = `${GAMES_JSON.length}`
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
